41ec054f1bdc451e74f8b84ca03868e70ad3b30a
SarangPratap <chamola31@gmail.com>
2026-01-09 22:00:36 +0100

    added wifi config

--- include/config.h
+++ include/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 // WiFi Configuration (user will update these)
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define WIFI_SSID "6a"
+#define WIFI_PASSWORD "thorsten!!!"
 
 // Backend Server Configuration
 #define BACKEND_HOST "192.168.1.100"  // User's laptop IP
